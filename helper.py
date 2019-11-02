0875d20a774723faad0bd1ea7700952d2a1b7345
brodude456 <fatihefeememisevic@gmail.com>
2019-11-02 15:22:46 +0100

    learning to do virtual stage and git

---
+++ helper.py
@@ -0,0 +1,28 @@
+string_board='''785 ∣ 439 ∣ 126 ∣ 
+        612 ∣ 875 ∣ 349 ∣ 
+        493 ∣ 621 ∣ 578 ∣ 
+        ------------------
+        357 ∣ 948 ∣ 261 ∣ 
+        861 ∣ 752 ∣ 934 ∣ 
+        904 ∣ 060 ∣ 005 ∣ 
+        ------------------
+        070 ∣ 300 ∣ 012 ∣ 
+        120 ∣ 007 ∣ 400 ∣ 
+        049 ∣ 206 ∣ 007 ∣ 
+        ------------------'''
+
+def make_board(str_br):
+    cleaned_str_rows=str_br.split("\n")
+    board=[]
+    for row in cleaned_str_rows:
+        if "∣" in row:
+            cleaned_str_column=row.split("∣")
+            list1=[]
+            for element in cleaned_str_column:
+                for number in element.strip():
+                    list1.append(int(number))
+            board.append(list1)
+    return board
+
+print(make_board(string_board))
+
---
+++ main.py
@@ -0,0 +1,104 @@
+from copy import deepcopy
+from helper import make_board
+
+string_board='''785 ∣ 439 ∣ 126 ∣ 
+    612 ∣ 875 ∣ 349 ∣ 
+    493 ∣ 621 ∣ 578 ∣ 
+    ------------------
+    357 ∣ 948 ∣ 261 ∣ 
+    861 ∣ 752 ∣ 934 ∣ 
+    904 ∣ 060 ∣ 005 ∣ 
+    ------------------
+    070 ∣ 300 ∣ 012 ∣ 
+    120 ∣ 007 ∣ 400 ∣ 
+    049 ∣ 206 ∣ 007 ∣ 
+    ------------------
+       '''
+
+board = make_board(string_board)
+
+def make_dict():
+
+    dict={}
+
+    for row in range(len(board)):
+        for column in range(len(board[0])):
+            dict[(row,column)]=[]
+
+    return dict
+
+def find_empty(bo):
+    for rowind,row in enumerate(bo):
+        for colind,col in enumerate(row):
+            if col==0:
+                return [rowind,colind]
+    return False
+
+#DID
+
+dict=make_dict()
+
+board_copy=deepcopy(board)
+
+emptyplaceslist=[]
+while find_empty(board):
+    emptyplaceslist.append(find_empty(board))
+    board[find_empty(board)[0]][find_empty(board)[1]]=1
+board=board_copy
+
+def solve(bo):
+    while True:
+        print_board(bo)
+        print("       ")
+        pos=find_empty(bo)
+        if not pos:
+            break
+        bruh=0
+        for num in range(1,10):
+            if valid(bo,num,pos) and num not in dict[tuple(pos)]:
+                bo[pos[0]][pos[1]]=num
+                bruh=1
+
+        if bruh==0:
+            dict[tuple(pos)]=[]
+
+            previus_not_filled_place=(emptyplaceslist[emptyplaceslist.index(pos)-1][0],emptyplaceslist[emptyplaceslist.index(pos)-1][1])
+
+            dict[previus_not_filled_place].append(bo[emptyplaceslist[emptyplaceslist.index(pos)-1][0]][emptyplaceslist[emptyplaceslist.index(pos)-1][1]])
+            bo[previus_not_filled_place[0]][previus_not_filled_place[1]]=0
+
+    return None
+
+def valid(bo, num, pos):
+
+    if num!=0:
+        for colind,col in enumerate(bo[pos[0]]):
+            if col==num and colind!=pos[1]:
+                return False
+        for rowind,row in enumerate(bo):
+            if row[pos[1]]==num and rowind!=pos[0]:
+                return False
+        for row in range(pos[0]//3*3,pos[0]//3*3+3):
+            for col in range(pos[1]//3*3,pos[1]//3*3+3):
+                if [row,col]!=pos:
+                    if bo[row][col]==num:
+                        return False
+    return True
+
+def print_board(bo):
+    for rowind,row in enumerate(bo):
+        for colind,column in enumerate(row):
+            print(column,end="")
+            if (colind+1)%3==0:
+                print(" ∣ ",end="")
+        print("")
+        if (rowind+1)%3==0:
+            print("-"*(len(bo[0])+3*3))
+
+#DID
+
+solve(board)
+print(board)
+
+
+
